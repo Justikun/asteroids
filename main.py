8ea1f689dbf1fb1953c0e6dc932588c0f13e9935
Justin Lowry <8211769+Justikun@users.noreply.github.com>
2025-08-31 19:20:28 -0600

    draw player

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 import pygame
 
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+
+    # Game Loop
     while True:
+        # Close Button
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
+        dt = pygame.time.Clock().tick(60) / 1000
         screen.fill("#000000")
-        pygame.display.flip()
 
+        player.draw(screen)
+
+        pygame.display.flip()
+    # End Game Loop
 
 
 
